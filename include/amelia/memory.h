e8103680da0abed390a5817aece86ae6e309e78a
Giully314 <giully@protonmail.com>
2025-02-14 19:46:43 +0100

    feat: add header file for low level memory declarations.

---
+++ include/amelia/memory.h
@@ -0,0 +1,25 @@
+// amelia/memory.h
+// PURPOSE:
+//
+// DESCRIPTION:
+//
+
+#ifndef _AMELIA_MEMORY_H
+#define _AMELIA_MEMORY_H
+
+#define PAGE_SHIFT 12
+#define TABLE_SHIFT 0
+#define SECTION_SHIFT (PAGE_SHIFT + TABLE_SHIFT)
+
+#define PAGE_SIZE (1 << PAGE_SHIFT)
+#define SECTION_SIZE (1 << SECTION_SHIFT)
+
+#define LOW_MEMORY (2 * SECTION_SIZE)
+
+#ifndef __ASSEMBLER__
+
+void memzero(unsigned long src, unsigned long num_bytes);
+
+#endif
+
+#endif  // _AMELIA_MEMORY_H
