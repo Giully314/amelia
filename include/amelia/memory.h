5f23a7f4fce465dacda5b4c49df5e83b5a5d0e0d
Giully314 <giully@protonmail.com>
2025-02-26 22:47:17 +0100

    fix: typo in the definition of TABLE_SHIFT, should be 9 and not 0.

--- include/amelia/memory.h
+++ include/amelia/memory.h
@@ -8,7 +8,7 @@
 #define _AMELIA_MEMORY_H
 
 #define PAGE_SHIFT 12
-#define TABLE_SHIFT 0
+#define TABLE_SHIFT 9
 #define SECTION_SHIFT (PAGE_SHIFT + TABLE_SHIFT)
 
 #define PAGE_SIZE (1 << PAGE_SHIFT)
