902c05d19713524cefce56f0baca8f0456e59a69
Giully314 <giully@protonmail.com>
2025-02-26 22:50:12 +0100

    refactor: clean while loop condition in send and recv uart.

--- src/peripherals/uart/mini_uart.c
+++ src/peripherals/uart/mini_uart.c
@@ -26,41 +26,33 @@ void uart_init() {
     delay(150);
     put32(GPPUDCLK0, 0);
 
+    // Initialize mini UART.
     put32(AUX_ENABLES, 1); // Enable mini uart (enable register access).
     put32(AUX_MU_CNTL_REG, 0); // Disable auto cflow control and disable receiver and transmitter for initialization.
     put32(AUX_MU_IER_REG, 0); // Disable receive and transmit interrupts (temporary).
     put32(AUX_MU_LCR_REG, 3); // Enable 8 bit mode.
     put32(AUX_MU_MCR_REG, 0); // Set RTS line to be always high.
     put32(AUX_MU_BAUD_REG, 270); // Set baud rate (symbol rate for communication channel) to 115200.
-
     put32(AUX_MU_CNTL_REG, 3); // Enable transmitter and receiver.
 }
 
 void uart_send(char c) {
     // This check if the bit 5 of the data status reg is set, so it can
     // accept atleast one byte to be sent.
-    while (1) {
-        if (get32(AUX_MU_LSR_REG) & 0x20) {
-            break;
-        }
-    }
+    while (!(get32(AUX_MU_LSR_REG) & 0x20));
     put32(AUX_MU_IO_REG, c);
 }
 
 char uart_recv() {
     // This check if the first bit of the data status reg is set, so it means
     // that the FIFO buffer has at least one byte that can be read. 
-    while (1) {
-        if (get32(AUX_MU_LSR_REG) & 0x01) {
-            break;
-        }
-    }
-    // Note that the data to be read are in the 
+    while (!(get32(AUX_MU_LSR_REG) & 0x01));
+    // Note that the data to be read are in the 8 bits of the register.
     return (char)(get32(AUX_MU_IO_REG) & 0xff);
 }
 
 void uart_send_string(const char* msg) {
     for (int i = 0; msg[i] != '\0'; ++i) {
-        // uart_send(msg[i]);
+        uart_send(msg[i]);
     }
 }
