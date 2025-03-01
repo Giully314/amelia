681c33b206c20a258420d8529a45886fa20e0363
Giully314 <giully@protonmail.com>
2025-03-01 13:00:11 +0100

    feat: change name of uart called functions.

--- src/kernel/start.c
+++ src/kernel/start.c
@@ -4,11 +4,11 @@
 #include <amelia/peripherals/uart/mini_uart.h>
 
 void kernel_main() {
-    uart_init();
+    mini_uart_init();
     
-    uart_send_string("hello world");
+    mini_uart_send_string("hello world\r\n");
 
     while (1) {
-        uart_send(uart_recv());
+        mini_uart_send(uart_recv());
     }
 }
