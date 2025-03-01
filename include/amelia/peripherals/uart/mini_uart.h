424d1d4e86bd06f32f80fb1b4af16918fa631de4
Giully314 <giully@protonmail.com>
2025-03-01 12:59:38 +0100

    feat: change function name header to mini_uart.

--- include/amelia/peripherals/uart/mini_uart.h
+++ include/amelia/peripherals/uart/mini_uart.h
@@ -1,28 +1,28 @@
 // amelia/peripherals/uart/mini_uart.h
-// PURPOSE: Implement basics for UART (universal async receiver-transmitter).
-//
-// STRUCTS:
+// PURPOSE: Implement basics for mini UART (universal async receiver-transmitter).
 //
 // DESCRIPTION:
 //  The Universal Asynchoronous Receiver-Transmitter is protocol for devices which
 //  uses async communication.
 //  https://en.wikipedia.org/wiki/Universal_asynchronous_receiver-transmitter
+//  The mini UART implementation has smaller FIFO, no control flow and it is more prone to lose 
+//  data at higher baudrate with respect to the PL011 (UART).
 //
 #ifndef _AMELIA_HARDWARE_MINI_UART_H
 #define _AMELIA_HARDWARE_MINI_UART_H
 
 // Initialize UART section.
-void uart_init();
+void mini_uart_init();
 
 
 // Send a byte.
-void uart_send(char c);
+void mini_uart_send(char c);
 
 // Receive a byte.
-char uart_recv();
+char mini_uart_recv();
 
 // Send a string.
 // Precondition: the string must be null terminated.
-void uart_send_string(const char* msg);
+void mini_uart_send_string(const char* msg);
 
 #endif  // _AMELIA_HARDWARE_MINI_UART_H
--- src/peripherals/uart/mini_uart.c
+++ src/peripherals/uart/mini_uart.c
@@ -4,7 +4,7 @@
 #include <amelia/peripherals/aux.h>
 #include <amelia/utils.h>
 
-void uart_init() {
+void mini_uart_init() {
     unsigned int selector;
 
     // This set the gpio14 to alternate function 5 txd1 (transimit data) and gpio15
@@ -26,24 +26,30 @@ void uart_init() {
     delay(150);
     put32(GPPUDCLK0, 0);
 
+    // Exercise 1.1.
+    const unsigned int system_clock_freq = 250; // 250 Mhz
+    // Bits/sec (equivalent to baud rate only if 2 symbols are used in the system, in this case 0 and 1).
+    const unsigned int baudrate = 115200; 
+    const unsigned int baudrate_reg = ((system_clock_freq / (baudrate * 8)) - 1) & 0xff;
+
     // Initialize mini UART.
     put32(AUX_ENABLES, 1); // Enable mini uart (enable register access).
     put32(AUX_MU_CNTL_REG, 0); // Disable auto cflow control and disable receiver and transmitter for initialization.
     put32(AUX_MU_IER_REG, 0); // Disable receive and transmit interrupts (temporary).
     put32(AUX_MU_LCR_REG, 3); // Enable 8 bit mode.
     put32(AUX_MU_MCR_REG, 0); // Set RTS line to be always high.
-    put32(AUX_MU_BAUD_REG, 270); // Set baud rate (symbol rate for communication channel) to 115200.
+    put32(AUX_MU_BAUD_REG, baudrate_reg); // Set baud rate register value (270 corresponds to 115200 bits/s, check manual for the computation)
     put32(AUX_MU_CNTL_REG, 3); // Enable transmitter and receiver.
 }
 
-void uart_send(char c) {
+void mini_uart_send(char c) {
     // This check if the bit 5 of the data status reg is set, so it can
     // accept atleast one byte to be sent.
     while (!(get32(AUX_MU_LSR_REG) & 0x20));
     put32(AUX_MU_IO_REG, c);
 }
 
-char uart_recv() {
+char mini_uart_recv() {
     // This check if the first bit of the data status reg is set, so it means
     // that the FIFO buffer has at least one byte that can be read. 
     while (!(get32(AUX_MU_LSR_REG) & 0x01));
@@ -51,8 +57,8 @@ char uart_recv() {
     return (char)(get32(AUX_MU_IO_REG) & 0xff);
 }
 
-void uart_send_string(const char* msg) {
+void mini_uart_send_string(const char* msg) {
     for (int i = 0; msg[i] != '\0'; ++i) {
-        uart_send(msg[i]);
+        mini_uart_send(msg[i]);
     }
 }
