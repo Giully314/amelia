ce6599a5dbc867d17aeefb64baec6d55c5ffe8a4
Giully314 <giully@protonmail.com>
2025-03-12 13:14:10 +0100

    feat: update start.

--- src/kernel/start.c
+++ src/kernel/start.c
@@ -1,41 +1,27 @@
 // src/kernel/start.c
 // DESCRIPTION: This file has methods for the start of the kernel.
 
-// #include <amelia/peripherals/uart/mini_uart.h>
-#include <amelia/peripherals/uart/pl011_uart.h>
+#include <amelia/peripherals/uart/mini_uart.h>
+#include <amelia/printf.h>
+#include <amelia/utils.h>
 
-static volatile int semaphore = 0;
+void putc(void *p, char c) { 
+    mini_uart_send(c);
+}
 
 // Function executed by the master process.
 void kernel_main(unsigned int processor_id) {
     if (processor_id == 0) {
-        uart_init();
+        init_printf(0, putc);
+        mini_uart_init();
     }
     char id = processor_id + '0'; // convert to ascii representation.
     
-    // Wait for the turn to print.
-    while (processor_id != semaphore);
-    
-    uart_send_string("hello world from ");
-    uart_send(id);
-    uart_send_string("\r\n");
-
-    // this should be an atomic instruction.n
-    ++semaphore;
+    printf("Hello from processor %c with current EL %u\n", id, get_el());
     
     if (processor_id == 0) {
         while (1) {
     
         }
     }
-    // uart_init();
-    // uart_send_string("hello world\n");
-    // while (1) {
-    //     uart_send(uart_recv());
-    // }
-}
-
-// This function is used 
-void print_hello() {
-
 }
