bc9a92219f1e34f66575268c1825bca3f23358ac
Giully314 <giully@protonmail.com>
2025-03-15 00:14:50 +0100

    feat: implementation of IRQ initialization and handler in EL1.

---
+++ include/amelia/peripherals/irq.h
@@ -0,0 +1,46 @@
+// amelia/peripherals/irq.h
+// PURPOSE: Define bit value for enabling/disabling interrupts.
+// 
+// DESCRIPTION:
+//  For now we work only with IRQ, that's why in irq.S we set 2 as a value for 
+//  the DAIF register. 
+//  D (bit 9): debug exception masked if set to 1.
+//  A (bit 8): SError exception masked if set to 1.
+//  I (bit 7): IRQ exception masked if set to 1.
+//  F (bit 6): FIQ exception masked if set to 1.
+//  Using daifclr and daifset we can clear/set directly the bits using values
+//  for the first 4 bits. Set is used for masking the exceptions.
+//  
+
+#ifndef _AMELIA_PERIPHERALS_IRQ_H
+#define _AMELIA_PERIPHERALS_IRQ_H
+
+#include <amelia/peripherals/peripherals_base.h>
+
+// Register for current pending interrupts.
+#define IRQ_PENDING_REG1 (PERIPHERALS_IRQ_START + 0x204)
+#define IRQ_PENDING_REG2 (PERIPHERALS_IRQ_START + 0x208)
+
+#define IRQ_ENABLE_REG1_CORE0 (PERIPHERALS_IRQ_START + 0x210)
+#define IRQ_ENABLE_REG2_CORE0 (PERIPHERALS_IRQ_START + 0x214)
+
+
+#define IRQ_SYSTEM_TIMER_0 (1 << 0)
+#define IRQ_SYSTEM_TIMER_1 (1 << 1)
+#define IRQ_SYSTEM_TIMER_2 (1 << 2)
+#define IRQ_SYSTEM_TIMER_3 (1 << 3)
+
+#ifndef __ASSEMBLER__ 
+
+
+void irq_init_vector_table();
+
+void irq_enable();
+void irq_disable();
+
+void irq_enable_interrupt_controller();
+
+
+#endif 
+
+#endif // _AMELIA_PERIPHERALS_IRQ_H
---
+++ src/peripherals/irq.c
@@ -0,0 +1,54 @@
+// src/peripherals/irq.c
+#include <amelia/peripherals/irq.h>
+#include <amelia/utils.h>
+#include <amelia/printf.h>
+#include <amelia/peripherals/timer.h>
+
+const char *entry_error_messages[] = {
+	"SYNC_INVALID_EL1t",
+	"IRQ_INVALID_EL1t",		
+	"FIQ_INVALID_EL1t",		
+	"ERROR_INVALID_EL1T",		
+
+	"SYNC_INVALID_EL1h",		
+	"IRQ_INVALID_EL1h",		
+	"FIQ_INVALID_EL1h",		
+	"ERROR_INVALID_EL1h",		
+
+	"SYNC_INVALID_EL0_64",		
+	"IRQ_INVALID_EL0_64",		
+	"FIQ_INVALID_EL0_64",		
+	"ERROR_INVALID_EL0_64",	
+
+	"SYNC_INVALID_EL0_32",		
+	"IRQ_INVALID_EL0_32",		
+	"FIQ_INVALID_EL0_32",		
+	"ERROR_INVALID_EL0_32"	
+};
+
+
+// Enable the IRQ on core 0.
+void irq_enable_interrupt_controller() {
+    put32(IRQ_ENABLE_REG1_CORE0, IRQ_SYSTEM_TIMER_1);
+}
+
+// Handle IRQ.
+void irq_handle() {
+    // For handling multiple interrupts in the same call,
+    // wrap this in a loop.
+    // Also the mechanism is not precise, because we need to
+    // extract each bit with an AND to check.
+    unsigned int irq = get32(IRQ_PENDING_REG1);
+    switch (irq) {
+        case (IRQ_SYSTEM_TIMER_1):
+            timer_handle_irq();
+            break;
+        default:
+            printf("Unkwown pending irq: %x\r\n", irq);
+    }
+}
+
+// Temporary function for dealing with invalid (not supported) exceptions.
+void show_invalid_entry_msg (int type, unsigned long esr, unsigned long address) {
+    printf("%s, ESR: %x, address: %x\r\n", entry_error_messages[type], esr, address);
+}
