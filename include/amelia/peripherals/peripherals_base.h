d828df6550f295d8539d0e534e4d950677eca9da
Giully314 <giully@protonmail.com>
2025-03-15 00:11:25 +0100

    feat: add start address for irq and timer.

--- include/amelia/peripherals/peripherals_base.h
+++ include/amelia/peripherals/peripherals_base.h
@@ -4,12 +4,24 @@
 // DESCRIPTION:
 //  This file describes the base informations needed by all the periphals
 //  such as memory start point.
-// These informations can be bound looking at the board manual BCM2837 ARM Peripherals
+//  These informations can be bound looking at the board manual BCM2837 ARM Peripherals.
+//  The start memory for peripherals depends on if we access memory directly or 
+//  using the bus.
+// 
 
 #ifndef _AMELIA_PERIPHERALS_BASE_H
 #define _AMELIA_PERIPHERALS_BASE_H
 
 // section 1.2.3
 #define PERIPHERALS_MEMORY_START 0x3f000000
+
+
+// section 7.5 
+// In the section the base address is reported as 0x7e00b000
+// but we use 3f for the arguments discussed in 1.2.3
+#define PERIPHERALS_IRQ_START 0x3f00b000
+
+// section 12
+#define PERIPHERALS_SYSTEM_TIMER_START 0x3f003000
 
 #endif  // _AMELIA_PERIPHERALS_BASE_H
