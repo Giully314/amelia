f51cee347842db2b77383fac69fd459dd6fbea87
Giully314 <giully@protonmail.com>
2025-04-02 01:09:23 +0200

    feat: utility min and max function between int.

---
+++ include/amelia/minmax.h
@@ -0,0 +1,18 @@
+// amelia/minmax.h
+// PURPOSE: Define min and max utility functions.
+
+#ifndef _AMELIA_MINMAX_H
+#define _AMELIA_MINMAX_H
+
+#include <amelia/basic_types.h>
+
+
+inline i64 min(i64 a, i64 b) {
+    return a < b ? a : b;
+}
+
+inline i64 max(i64 a, i64 b) {
+    return a > b ? a : b;
+}
+
+#endif // _AMELIA_MINMAX_H
