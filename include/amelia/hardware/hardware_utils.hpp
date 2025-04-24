ff2a2247d54476b7167149211523151c946e90be
Giully314 <giully@protonmail.com>
2025-04-24 13:03:56 +0200

    feat: add set and clear bits at the given address.

--- include/amelia/hardware/hardware_utils.hpp
+++ include/amelia/hardware/hardware_utils.hpp
@@ -8,15 +8,21 @@
 extern "C" {
 
 // Wait for n nanoseconds.
-void delay(amelia::u64 nanoseconds);
+auto delay(amelia::u64 nanoseconds) -> void;
 
 // Get value of 4 bytes at address.
-amelia::u32 mem_read32(amelia::u64 address);
+auto mem_read32(amelia::u64 address) -> amelia::u32;
 
 // Put a value into location specified by address.
-void mem_write32(amelia::u64 address, amelia::u32 value);
+auto mem_write32(amelia::u64 address, amelia::u32 value) -> void;
+
+// Set to 1 the bits specified by value at the given address.
+auto mem_set32(amelia::u64 address, amelia::u32 value) -> void;
+
+// Set to 0 the bits specified by value at the given address.
+auto mem_clear32(amelia::u64 address, amelia::u32 value) -> void;
 
 // Get current exception level.
-amelia::u32 get_el();
+auto get_el() -> amelia::u32;
 
 } // extern C
